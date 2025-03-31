92d6ebfad4e82fbad09adb56bc36d592d47dbf44
Jatin Foujdar <jatin321001@gmail.com>
2025-04-01 01:49:23 +0530

    Initial Commit

---
+++ Blink-IOS-App/Blink_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blink_IOS_AppApp.swift
+//  Blink-IOS-App
+//
+//  Created by jatin foujdar on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Blink_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blink-IOS-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blink-IOS-App
+//
+//  Created by jatin foujdar on 01/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
